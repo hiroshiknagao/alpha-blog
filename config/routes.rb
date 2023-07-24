cbcd26ec0c07a347dac8877b1cfe1d0da1079436
Hiroshi Nagao <hi_1205@hotmail.com>
2023-07-24 21:06:05 +0900

    add article listing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   get 'migration/create_articles'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index]
   
 end
