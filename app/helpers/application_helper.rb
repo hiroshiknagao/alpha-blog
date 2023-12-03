fc96aa07646ce6198ac7beb82f47b33155b4bafb
Hiroshi Nagao <hi_1205@hotmail.com>
2023-12-03 10:50:27 +0900

    add pagination

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     def gravator_for(user, option = { size: 80})
         email_address = user.email.downcase
         hash = Digest::MD5.hexdigest(email_address)
-        size =  option[:size]
+        size = option[:size]
         gravator_url = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
         image_tag(gravator_url, alt: user.username, class:"rounded mx-auto shadow d-block")
     end
